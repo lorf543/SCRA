a4638ed7db9cf279538aec7d25a7d323c3d3fbdd
Eduardo Sanchez <80008158+lorf543@users.noreply.github.com>
2024-06-17 10:56:57 -0400

    static

--- SCRA/settings.py
+++ SCRA/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import environ
+import os
 
 
 env = environ.Env( DEBUG=(bool, False) )
@@ -147,7 +148,7 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [ BASE_DIR / 'static']
 
-MEDIA_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 # Default primary key field type
---
+++ staticfiles/scripts/main.js
@@ -0,0 +1,89 @@
+new DataTable('#scraList',{ 
+  "autoWidth": true,
+  "scrollX":true,
+  "scrollY":600,
+});
+
+new DataTable('#trackerReturn',{ 
+  "autoWidth": true,
+  "scrollX":true,
+  "scrollY":600,
+});
+
+new DataTable('#tableInbox',{ 
+  "autoWidth": true,
+  "scrollX":true,
+  "scrollY":600,
+});
+new DataTable('#Pendinglist',{ 
+  "autoWidth": true,
+  "scrollX":true,
+  "scrollY":600,
+});
+
+// new flatpickr("#test_id", {});
+
+//Table for SCRA candidates
+// $(document).ready(function() {
+//   var table = $('#scraList').DataTable({
+//       "paging": true,
+//       "lengthChange": true,
+//       "searching": true,
+//       "ordering": true,
+//       "info": true,
+//       "autoWidth": true,
+//       "scrollX": true,
+//       "scrollY": 600,
+//       "columnDefs": [
+//           // Add any specific column definitions if needed
+//       ]
+//   });   
+// });
+
+
+
+window.addEventListener('DOMContentLoaded', event => {
+
+  // Toggle the side navigation
+  const sidebarToggle = document.body.querySelector('#sidebarToggle');
+  if (sidebarToggle) {
+      // Uncomment Below to persist sidebar toggle between refreshes
+      // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+      //     document.body.classList.toggle('sb-sidenav-toggled');
+      // }
+      sidebarToggle.addEventListener('click', event => {
+          event.preventDefault();
+          document.body.classList.toggle('sb-sidenav-toggled');
+          localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+      });
+  };
+
+ 
+    // Get all rows with the class 'customer-row'
+    var customerRows = document.querySelectorAll('.customer-row');
+
+    // Add a click event listener to each row
+    customerRows.forEach(function(row) {
+      row.addEventListener('click', function() {
+        // Remove the 'selected' class from all rows
+        customerRows.forEach(function(innerRow) {
+          innerRow.classList.remove('selected');
+        });
+  
+        // Add the 'selected' class to the clicked row
+        row.classList.add('selected');
+      });
+    });
+    
+});
+
+
+
+
+
+
+
+
+
+
+
