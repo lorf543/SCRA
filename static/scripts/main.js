698d8c40a91e171e08a4d9734cc30d2816332cea
Eduardo Sanchez <80008158+lorf543@users.noreply.github.com>
2024-06-24 22:15:18 -0400

    new look

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,24 +1,24 @@
-new DataTable('#scraList',{ 
+new DataTable('#scraList', {
   "autoWidth": true,
-  "scrollX":true,
-  "scrollY":600,
+  "scrollX": true,
+  "scrollY": 600,
 });
 
-new DataTable('#trackerReturn',{ 
+new DataTable('#trackerReturn', {
   "autoWidth": true,
-  "scrollX":true,
-  "scrollY":600,
+  "scrollX": true,
+  "scrollY": 600,
 });
 
-new DataTable('#tableInbox',{ 
+new DataTable('#tableInbox', {
   "autoWidth": true,
-  "scrollX":true,
-  "scrollY":600,
+  "scrollX": true,
+  "scrollY": 600,
 });
-new DataTable('#Pendinglist',{ 
+new DataTable('#Pendinglist', {
   "autoWidth": true,
-  "scrollX":true,
-  "scrollY":600,
+  "scrollX": true,
+  "scrollY": 600,
 });
 
 // new flatpickr("#test_id", {});
@@ -47,34 +47,52 @@ window.addEventListener('DOMContentLoaded', event => {
   // Toggle the side navigation
   const sidebarToggle = document.body.querySelector('#sidebarToggle');
   if (sidebarToggle) {
-      // Uncomment Below to persist sidebar toggle between refreshes
-      // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
-      //     document.body.classList.toggle('sb-sidenav-toggled');
-      // }
-      sidebarToggle.addEventListener('click', event => {
-          event.preventDefault();
-          document.body.classList.toggle('sb-sidenav-toggled');
-          localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
-      });
+    // Uncomment Below to persist sidebar toggle between refreshes
+    // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+    //     document.body.classList.toggle('sb-sidenav-toggled');
+    // }
+    sidebarToggle.addEventListener('click', event => {
+      event.preventDefault();
+      document.body.classList.toggle('sb-sidenav-toggled');
+      localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+    });
   };
 
- 
-    // Get all rows with the class 'customer-row'
-    var customerRows = document.querySelectorAll('.customer-row');
 
-    // Add a click event listener to each row
-    customerRows.forEach(function(row) {
-      row.addEventListener('click', function() {
-        // Remove the 'selected' class from all rows
-        customerRows.forEach(function(innerRow) {
-          innerRow.classList.remove('selected');
-        });
-  
-        // Add the 'selected' class to the clicked row
-        row.classList.add('selected');
+  // Get all rows with the class 'customer-row'
+  var customerRows = document.querySelectorAll('.customer-row');
+
+  // Add a click event listener to each row
+  customerRows.forEach(function (row) {
+    row.addEventListener('click', function () {
+      // Remove the 'selected' class from all rows
+      customerRows.forEach(function (innerRow) {
+        innerRow.classList.remove('selected');
       });
+
+      // Add the 'selected' class to the clicked row
+      row.classList.add('selected');
     });
-    
+  });
+
+});
+
+
+$(".menu > ul > li").click(function (e) {
+  // remove active from already active
+  $(this).siblings().removeClass("active");
+  // add active to clicked
+  $(this).toggleClass("active");
+  // if has sub menu open it
+  $(this).find("ul").slideToggle();
+  // close other sub menu if any open
+  $(this).siblings().find("ul").slideUp();
+  // remove active class of sub menu items
+  $(this).siblings().find("ul").find("li").removeClass("active");
+});
+
+$(".menu-btn").click(function () {
+  $(".sidebar").toggleClass("active");
 });
 
 
